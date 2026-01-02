fe073db8162d552838fd8b39c29fcdf4157568e3
CuongBC195 <cuongbc.work@gmail.com>
2026-01-02 20:39:41 +0700

    Fix: Disable webpack cache in dev mode to prevent chunk loading errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable webpack cache to avoid chunk loading issues
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
